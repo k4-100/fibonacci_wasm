f2610e3574e42002ab0fe970efdd74cfb1f82df6
k4-100 <k4-100@protonmail.com>
2022-01-01 14:40:35 +0100

    Added console_log to wasm

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-import init, {greet} from "./pkg/hello_wasm.js"
+import init, {greet, console_log} from "./pkg/hello_wasm.js"
+
 
 
 init()
 .then(() => {
-    greet("WebAssembly")
+    console_log( "Hello WASM")
 })
 .catch(
     err => console.log('WASM ERROR: ', err)
--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,16 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern{
     pub fn alert( s: &str );
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log( s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet( name : &str ){
     alert(&format!("Hello, {}!", name ));
+}
+
+#[wasm_bindgen]
+pub fn console_log( s : &str){
+    log( s );
 }
