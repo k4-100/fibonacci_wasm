2a0f06df04ad27984b02394d90234a3902061410
k4-100 <k4-100@protonmail.com>
2022-01-01 15:55:37 +0100

    Rework in display_fibonacci_seq()

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 import init, {greet, console_log, display_fibonacci_seq} from "./pkg/hello_wasm.js"
 
 
-
 init()
 .then(() => {
     console_log( "Hello WASM")
-    display_fibonacci_seq( 10 )
-
+    display_fibonacci_seq( 20 )
 })
 .catch(
     err => console.log('WASM ERROR: ', err)
--- src/utl.rs
+++ src/utl.rs
@@ -3,14 +3,16 @@
 pub fn get_fibonacci_string( count : usize ) -> String {
     let mut seq : Vec<u32> = vec![0,1];
     let mut f_str: String = String::new();
-    
-    for x in 2..count{
-        seq.push( seq[x-2] + seq[x-1] );
-    }
-    
-    seq.iter().for_each(
-        |n| f_str.push_str( format!("{}\n", n).as_str() )
+    f_str.push_str(
+        format!("0: {}\n1: {}\n", seq[0],seq[1]).as_str()
     );
+
+    for x in 2..2+count{
+        seq.push( seq[x-2] + seq[x-1] );
+        f_str.push_str( 
+            format!("{}: {}\n" ,x,seq[x]).as_str()
+        );
+    }
 
     return f_str;
 }
